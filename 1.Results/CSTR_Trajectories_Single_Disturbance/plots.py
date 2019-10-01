2c4fe1b6cec2d5cad98a0aff167625b7d034fbf6
Rui Nian <rnian@ualberta.ca>
2019-09-30 18:35:01 -0600

    updates

--- 1.Results/CSTR_Trajectories_Single_Disturbance/plots.py
+++ 1.Results/CSTR_Trajectories_Single_Disturbance/plots.py
@@ -1,6 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+fonts = {"family": "serif",
+         "weight": "normal",
+         "size": "12"}
+
+plt.rc('font', **fonts)
+plt.rc('text', usetex=True)
+
 # Load Data
 x_rl_1_mpccost = np.loadtxt('x_rl_1_mpccost.txt')
 u_rl_1_mpccost = np.loadtxt('u_rl_1_mpccost.txt')
@@ -24,29 +31,29 @@ u_mpc_5 = np.loadtxt('u_mpc_5.txt')
 State Trajectories
 """
 
-plt.rcParams["figure.figsize"] = (8, 5)
+plt.rcParams["figure.figsize"] = (16, 10)
 
 plt.subplot(2, 2, 1)
 plt.ylabel("Concentration of Reactant A")
 plt.title("State Trajectories")
-plt.plot(x_rl_1_mpccost[0:15, 0], label='RL (MPC Cost)')
-plt.plot(x_rl_1_rlcost[0:15, 0], label='RL (RL Cost)')
-# plt.plot(x_rl_5[0:15, 0], label='RL (Evaluate time: 5)')
-plt.plot(x_mpc_1[0:15, 0], label='MPC')
-# plt.plot(x_mpc_5[0:15, 0], label='MPC (Evaluation time: 5)')
-plt.plot(x_tabularrl_mpccost[0:15, 0], label='Tabular RL (MPC Cost)')
-plt.legend()
+# plt.plot(x_rl_1_mpccost[0:15, 0], label='Deep RL (MPC Cost)')
+# plt.plot(x_rl_1_rlcost[0:15, 0], label='Deep RL (RL Cost)')
+plt.plot(x_rl_5[0:15, 0], label='Deep RL (Evaluate time: 5)')
+# plt.plot(x_mpc_1[0:15, 0], label='MPC')
+plt.plot(x_mpc_5[0:15, 0], label='MPC (Evaluation time: 5)')
+plt.plot(x_tabularrl_mpccost[0:15, 0], label='Tabular RL (Evaluation time: 5)')
+plt.legend(frameon=False)
 
 plt.subplot(2, 2, 3)
-plt.ylabel("Reactor Temperature, T (°C)")
-plt.xlabel("Time, T (min)")
-plt.plot(x_rl_1_mpccost[0:15, 1], label='RL (MPC Cost)')
-plt.plot(x_rl_1_rlcost[0:15, 1], label='RL (RL Cost)')
-# plt.plot(x_rl_5[0:15, 1], label='RL (Evaluate time: 5)')
-plt.plot(x_mpc_1[0:15, 1], label='MPC')
-# plt.plot(x_mpc_5[0:15, 1], label='MPC (Evaluation time: 5)')
-plt.plot(x_tabularrl_mpccost[0:15, 1], label='Tabular RL (MPC Cost)')
-plt.legend()
+plt.ylabel(r"Reactor Temperature, \textit{T} (°C)")
+plt.xlabel(r"Time, \textit{t} (min)")
+# plt.plot(x_rl_1_mpccost[0:15, 1], label='Deep RL (MPC Cost)')
+# plt.plot(x_rl_1_rlcost[0:15, 1], label='Deep RL (RL Cost)')
+plt.plot(x_rl_5[0:15, 1], label='Deep RL (Evaluate time: 5)')
+# plt.plot(x_mpc_1[0:15, 1], label='MPC')
+plt.plot(x_mpc_5[0:15, 1], label='MPC (Evaluation time: 5)')
+plt.plot(x_tabularrl_mpccost[0:15, 1], label='Tabular RL (Evaluation time: 5)')
+plt.legend(frameon=False)
 
 # plt.subplot(313)
 # plt.plot(x_rl_1_mpccost[0:10, 2], label='test')
@@ -60,27 +67,30 @@ Input Trajectories
 
 plt.subplot(2, 2, 2)
 plt.title("Input Trajectories")
-plt.ylabel("Coolant Temperature, T (°C)")
-plt.plot(u_rl_1_mpccost[0:15, 0], label='RL (MPC Cost)')
-plt.plot(u_rl_1_rlcost[0:15, 0], label='RL (RL Cost)')
-# plt.plot(u_rl_5[0:15, 0], label='RL (Evaluate time: 5)')
-plt.plot(u_mpc_1[0:15, 0], label='MPC')
-# plt.plot(u_mpc_5[0:15, 0], label='MPC (Evaluation time: 5)')
-plt.plot(u_tabularrl_mpccost[0:15, 0], label='Tabular RL (MPC Cost)')
+plt.ylabel(r"Coolant Temperature, \textit{T} (°C)")
+# plt.plot(u_rl_1_mpccost[0:15, 0], label='Deep RL (MPC Cost)')
+# plt.plot(u_rl_1_rlcost[0:15, 0], label='Deep RL (RL Cost)')
+plt.plot(u_rl_5[0:15, 0], label='Deep RL (Evaluate time: 5)')
+# plt.plot(u_mpc_1[0:15, 0], label='MPC')
+plt.plot(u_mpc_5[0:15, 0], label='MPC (Evaluation time: 5)')
+plt.plot(u_tabularrl_mpccost[0:15, 0], label='Tabular RL (Evaluation time: 5)')
 plt.axis([0, 15, 299, 302.5])
-plt.legend()
+plt.legend(frameon=False)
 
 plt.subplot(2, 2, 4)
-plt.ylabel("Reactant Height, m (m)")
-plt.xlabel("Time, T (min)")
-plt.plot(u_rl_1_mpccost[0:15, 1], label='RL (MPC Cost)')
-plt.plot(u_rl_1_rlcost[0:15, 1], label='RL (RL Cost)')
-# plt.plot(u_rl_5[0:15, 1], label='RL (Evaluate time: 5)')
-plt.plot(u_mpc_1[0:15, 1], label='MPC')
-# plt.plot(u_mpc_5[0:15, 1], label='MPC (Evaluation time: 5)')
-plt.plot(u_tabularrl_mpccost[0:15, 1], label='Tabular RL (MPC Cost)')
+plt.ylabel(r"Reactant Height, \textit{m} (m)")
+plt.xlabel(r"Time, \textit{t} (min)")
+# plt.plot(u_rl_1_mpccost[0:15, 1], label='Deep RL (MPC Cost)')
+# plt.plot(u_rl_1_rlcost[0:15, 1], label='Deep RL (RL Cost)')
+plt.plot(u_rl_5[0:15, 1], label='Deep RL (Evaluate time: 5)')
+# plt.plot(u_mpc_1[0:15, 1], label='MPC')
+plt.plot(u_mpc_5[0:15, 1], label='MPC (Evaluation time: 5)')
+plt.plot(u_tabularrl_mpccost[0:15, 1], label='Tabular RL (Evaluation time: 5)')
 
-plt.legend()
+plt.legend(frameon=False)
+
+# plt.savefig('States_and_Inputs_1_CSTR.pdf', format='pdf', dpi=1000)
+plt.savefig('States_and_Inputs_5_CSTR.pdf', format='pdf', dpi=1000)
 
 plt.show()
 
